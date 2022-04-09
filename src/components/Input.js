96e9d2e6f4f84c38726bf3fe6f9420f6f0584bce
stephen <henblackstep@gmail.com>
2022-04-09 08:55:10 +0000

    [UPDATE] input labed on type, BuyTicket page form aligned

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-export const Input = ({ children, label, ...props }) => {
-  return (
+export const Input = ({ children, label, inputType = 0, ...props }) => {
+  return inputType === 0 ? (
     <label>
       <input {...props} />
       {label}
       {children}
     </label>
+  ) : (
+    <div className="py-1">
+      <label>{label}</label>
+      <input {...props} />
+    </div>
   );
 };
--- src/pages/BuyTicket.js
+++ src/pages/BuyTicket.js
@@ -5,6 +5,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { Input, Button } from "../components";
 import { showsData } from "../data/show";
 import { ChartContext } from "../context/user-context";
+import { currencyFormatter } from "../util/helper";
 
 export const BuyTicket = () => {
   const { chart, setChart } = useContext(ChartContext);
@@ -52,9 +53,13 @@ export const BuyTicket = () => {
     <div className="grid grid-cols-3 mx-4">
       <div className="p-4 shadow-sm ring-1 ring-slate-100">
         <h2 className="font-bold text-lg">Show Details</h2>
-        <h4>{show?.title}</h4>
-        <p>venue: {show?.venue}</p>
-        <p>date: {show?.date?.toDateString()}</p>
+        <img src={show?.image} alt="show thumnail" className="w-full" />
+        <div>
+          <p>venue: {show?.venue}</p>
+          <p>date: {show?.date?.toDateString()}</p>
+        </div>
+
+        <p>unit price: {currencyFormatter(show?.price)}</p>
       </div>
       <div className="col-span-2 p-4 m-auto">
         <h1 className="capitalize font-bold text-2xl mb-2 ">
@@ -71,11 +76,12 @@ export const BuyTicket = () => {
               className="w-full rounded-sm"
               name="userFullName"
               onChange={handleChange}
+              placeholder="firstName lastName otherName"
               value={userData.userFullName}
             />
           </div>
           <h2>ticket information</h2>
-          <div className="shadow-md rounded-md mb-2 py-2">
+          <div className="shadow-md rounded-md mb-2 p-4">
             <label htmlFor="ticketQuantity" className="capitalize">
               ticket quantity
             </label>
@@ -83,14 +89,17 @@ export const BuyTicket = () => {
               type="number"
               className="w-full "
               name="ticketQuantity"
+              placeholder="2"
               onChange={handleChange}
               value={userData.ticketQuantity}
             />
           </div>
           <h2>billing information</h2>
-          <div className="shadow-md rounded-md mb-2">
+          <div className="shadow-md rounded-md mb-2 p-4">
             <Input
               type="Number"
+              label="credit card number"
+              inputType={1}
               className="w-full rounded-sm"
               name="cardNumber"
               onChange={handleChange}
@@ -99,13 +108,17 @@ export const BuyTicket = () => {
             <div className="w-full flex space-x-2 mt-2">
               <Input
                 type="number"
+                label="ccv"
+                inputType={1}
                 className="w-full rounded-sm"
                 name="ccv"
                 onChange={handleChange}
                 value={userData.ccv}
               />
               <Input
+                label="expiry date"
                 type="month"
+                inputType={1}
                 className="w-full rounded-sm"
                 name="cardExpiry"
                 onChange={handleChange}
